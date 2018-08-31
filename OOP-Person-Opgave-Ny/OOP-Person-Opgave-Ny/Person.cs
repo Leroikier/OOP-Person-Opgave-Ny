fa69e1b5eb7b084601ec5bd073291fc0f37b816f
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-31 10:27:16 +0200

    <Færdigt resultat>

---
+++ OOP-Person-Opgave-Ny/OOP-Person-Opgave-Ny/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Person_Opgave_Ny
+{
+    class Person
+    {
+        public string Fornavn;
+        public string Efternavn;
+        public string Adresse;
+        public int PostNr;
+        public string By;
+
+        
+
+        public string UdskrivPerson()
+        {
+            return Fornavn + " " + Adresse;
+        }
+    }
+}
---
+++ OOP-Person-Opgave-Ny/OOP-Person-Opgave-Ny/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Person_Opgave_Ny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person { Fornavn = "Jens", Efternavn = "Hansen", Adresse = "Nybrovej 14", PostNr = 2800, By = "Kattestrup" };
+
+            Console.WriteLine(p1.UdskrivPerson());
+
+            Console.ReadKey();
+        }
+    }
+}
